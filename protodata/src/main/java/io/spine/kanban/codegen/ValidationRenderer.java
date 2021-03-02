38661c601ee2b64d44abf3972a4267f405ffef5f
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2021-03-02 19:05:05 +0200

    Add a skeleton for a ProtoData module

---
+++ protodata/src/main/java/io/spine/kanban/codegen/ValidationRenderer.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.kanban.codegen;
+
+import com.google.common.collect.ImmutableSet;
+import io.spine.protodata.renderer.Renderer;
+import io.spine.protodata.renderer.SourceFile;
+import io.spine.protodata.renderer.SourceSet;
+import io.spine.protodata.subscriber.CodeEnhancement;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public final class ValidationRenderer extends Renderer {
+
+    @NotNull
+    @Override
+    public SourceSet render(@NotNull SourceSet sources) {
+        ImmutableSet.Builder<SourceFile> files = ImmutableSet.builder();
+        for (CodeEnhancement enhancement : getEnhancements()) {
+            if (enhancement instanceof CheckFieldIsSet) {
+                CheckFieldIsSet checkIsSet = (CheckFieldIsSet) enhancement;
+                apply(checkIsSet, sources).ifPresent(files::add);
+            }
+        }
+        return new SourceSet(files.build());
+    }
+
+    private Optional<SourceFile> apply(CheckFieldIsSet enhancement, SourceSet sources) {
+        System.out.println(enhancement);
+        System.out.println(sources);
+        return Optional.empty();
+    }
+}
