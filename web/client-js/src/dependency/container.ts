057f53b0f19e2cc7b64fc10f3f0be15cdd05a102
tripolkaandrey <andrii.trypilka@nure.ua>
2022-09-08 14:10:03 +0300

    Configure Spine client in dependency container

---
+++ web/client-js/src/dependency/container.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import {ActorProvider, Client, init} from "spine-web/index";
+import KanbanModel from "proto/index";
+import SpineWebTypes from "spine-web/proto/index";
+import host from "@/dependency/spine/host"
+import Firebase from "firebase";
+import config from "@/dependency/firebase/config";
+
+const firebaseApp = Firebase.initializeApp(config);
+
+export const actorProvider = new ActorProvider();
+export const client: Client = init({
+    protoIndexFiles: [KanbanModel, SpineWebTypes],
+    endpointUrl: host,
+    firebaseDatabase: firebaseApp.database(),
+    actorProvider: actorProvider
+});
